24d54260ec242b4d1ac0de9a065af23114a79f71
fernandezche-1 <fernandezche166@gmail.com>
2026-02-09 14:16:40 -0600

    memorias.py

---
+++ memoriadinamica.py
@@ -0,0 +1,12 @@
+frutas = []
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uvas")
+print("Lista inicial:", frutas)
+
+frutas.pop(0)      
+frutas.pop(1)      
+frutas.append("Sandía")
+print("Lista final:", frutas)
---
+++ memoriaestatica.py
@@ -0,0 +1,6 @@
+calificaciones = [0] * 5
+
+for i in range(5):
+    calificaciones[i] = int(input(f"Captura la calificación {i + 1}: "))
+
+print("Calificaciones almacenadas:", calificaciones)
